4d75d0b21b655b29f4ef5e05c10ec2ddc9c24c0a
del10g <delledu35@gmail.com>
2023-04-27 16:49:27 +1000

    Moving window to lubuntu for laptop issue.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-with open("a_list.txt", "r") as file:
-    numbers = file.readlines()
-    print(set(list(numbers)))
-    
+import turtle
+colors = ["red", "green", "blue", "yellow", "orange", "purple"]
+t = turtle.Pen()
+turtle.bgcolor("black")
+t.speed(10)
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x//200+1)
+    t.forward(x)
+    t.left(59)
